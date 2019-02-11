992b45f3d4698ebd9d51e0b849dc554e933453bf
Gavin Stark <gavin@gstark.com>
2019-02-11 15:32:27 -0500

    Correctly assigns access token, and builds email addresses

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,20 @@
 class User < ApplicationRecord
-  belongs_to :person, optional: true
+  belongs_to :person, dependent: :destroy
 
   def self.from_omniauth(authentication_data)
     User.where(provider: authentication_data['provider'],uid: authentication_data['uid']).first_or_create do |user|
+      Rails.logger.debug ["authentication_data", authentication_data]
       person = user.build_person
 
+      # info.image
+
       person.full_name  = authentication_data.info.name
-      user.nickname     = authentication_data.info.nickname
-      user.access_token = authentication_data.info.access_token
+      person.nickname   = authentication_data.info.nickname
+      user.access_token = authentication_data.credentials.token
+
+      authentication_data.extra.all_emails.each do |github_email|
+        person.emails.build(label: "github", address: github_email.email, is_primary: github_email.primary)
+      end
     end
   end
 end
