b7104f9648fe062962dc01a1ea418eb739397af0
Gavin Stark <gavin@gstark.com>
2019-02-11 19:14:24 -0500

    Enforces null option for graphql types

--- app/graphql/types/user.rb
+++ app/graphql/types/user.rb
@@ -2,7 +2,7 @@ module Types
   class User < Types::BaseObject
     description "User"
 
-    field :person, [Types::Person]
+    field :person, [Types::Person], null: false
 
     field :is_admin, Boolean, null: false
   end
