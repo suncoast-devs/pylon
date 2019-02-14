7f479efb3d40060ed4c8f13c406b9eb147889dc7
Gavin Stark <gavin@gstark.com>
2019-02-14 16:11:34 -0500

    Decreases scope for github oauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   app_id = ENV.fetch('GITHUB_APP_ID', Rails.application.credentials.GITHUB_APP_ID)
   app_secret = ENV.fetch('GITHUB_APP_SECRET', Rails.application.credentials.GITHUB_APP_SECRET)
-  provider :github, app_id, app_secret, scope: "user,repo,gist"
+  provider :github, app_id, app_secret, scope: "read:user"
 end
