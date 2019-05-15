c19f4d8efcc424355ac5dbcccc904cb784dab969
Gavin Stark <gavin@gstark.com>
2019-05-14 21:27:43 -0400

    Adds the assignment URL to the nexus slack message

--- app/jobs/assignment_change_job.rb
+++ app/jobs/assignment_change_job.rb
@@ -2,16 +2,18 @@ class AssignmentChangeJob < ApplicationJob
   queue_as :default
 
   def perform(assignment:, type:)
+    url = "https://github.com/#{assignment.person.github}/#{assignment.person.assignments_repo}/issues/#{assignment.issue}"
+
     case type
     when "graded"
       GithubIssueInterface.comment_on_assignment(assignment)
-      PylonBot.message(assignment.person, %{Your homework *#{assignment.homework.title}* was marked: *#{assignment.score_description}*})
+      PylonBot.message(assignment.person, %{Your homework *#{assignment.homework.title}* was marked: *#{assignment.score_description}*\n#{url}})
     when "updated"
       GithubIssueInterface.update(assignment)
-      PylonBot.message(assignment.person, %{Your assignment *"#{assignment.homework.title}"* was updated with new content.})
+      PylonBot.message(assignment.person, %{Your assignment *"#{assignment.homework.title}"* was updated with new content.\n#{url}})
     when "created"
       GithubIssueInterface.create(assignment)
-      PylonBot.message(assignment.person, %{You have an new assignment: *"#{assignment.homework.title}"*})
+      PylonBot.message(assignment.person, %{You have an new assignment: *"#{assignment.homework.title}"*\n#{url}})
     end
   end
 end
