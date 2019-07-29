b2d99d624589ea65b5131b1798265926a834aa26
Gavin Stark <gavin@gstark.com>
2019-07-29 14:40:52 -0400

    Hack to make the default page size 999
    This is because our client isn't always smart about loading ALL the data we want

--- app/resources/application_resource.rb
+++ app/resources/application_resource.rb
@@ -7,7 +7,12 @@ class ApplicationResource < Graphiti::Resource
   self.abstract_class = true
   self.adapter = Graphiti::Adapters::ActiveRecord
   self.base_url = Rails.application.routes.default_url_options[:host]
-  self.endpoint_namespace = '/api/v1'
+  self.endpoint_namespace = "/api/v1"
+
+  # Make the default pagination very large (for now)
+  # HACK: a bit of a hack to get around not being great
+  #       at pagination in the client in all cases
+  self.default_page_size = 999
 
   def admin?
     current_user.is_admin?
