622ba746791846c46e960e1464ef313295a58ed5
Gavin Stark <gavin@gstark.com>
2019-09-25 12:13:41 -0400

    Adds query filter for active cohorts

--- app/resources/cohort_resource.rb
+++ app/resources/cohort_resource.rb
@@ -19,6 +19,16 @@ class CohortResource < ApplicationResource
     false
   end
 
+  filter :active, :boolean do
+    eq do |scope, value|
+      if value
+        scope.where("start_date <= :today and end_date >= :today", today: Date.today)
+      else
+        scope.where.not("start_date <= :today and end_date >= :today", today: Date.today)
+      end
+    end
+  end
+
   # many_to_many :units
   extra_attribute :units, :array do
     @object.units.to_a
