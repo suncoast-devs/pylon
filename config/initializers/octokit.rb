a8b1a59373d809f9d9d30004cb799308a5b236de
Gavin Stark <gavin@gstark.com>
2019-09-25 10:42:39 -0400

    Makes github requests automatically paginate though all results
    Fixes pylon issue #9

--- config/initializers/octokit.rb
+++ config/initializers/octokit.rb
@@ -1,2 +1,8 @@
-Octokit.client_id = ENV.fetch('GITHUB_APP_ID', Rails.application.credentials.GITHUB_APP_ID)
-Octokit.client_secret = ENV.fetch('GITHUB_APP_SECRET', Rails.application.credentials.GITHUB_APP_SECRET)
+Octokit.client_id = ENV.fetch("GITHUB_APP_ID", Rails.application.credentials.GITHUB_APP_ID)
+Octokit.client_secret = ENV.fetch("GITHUB_APP_SECRET", Rails.application.credentials.GITHUB_APP_SECRET)
+
+# Automatically paginate through results, pulling back all the data
+# for any github request.
+Octokit.configure do |config|
+  config.auto_paginate = true
+end
