185cffc5a451b92e93a36a1edeea23838756763a
Gavin Stark <gavin@gstark.com>
2020-05-12 13:34:28 -0400

    Ooops, wrong place for config

--- config/application.rb
+++ config/application.rb
@@ -29,5 +29,14 @@ module Pylon
     config.session_store :cookie_store, key: Rails.application.secrets.secret_key_base
     config.middleware.use ActionDispatch::Cookies
     config.middleware.use ActionDispatch::Session::CookieStore, Rails.application.config.session_options
+
+    # Configure Rails to use the Async adapter but only process one job at a time
+    # This sould be fine for production at this point but we could/should switch
+    # to something like sidekiq when we can
+    config.active_job.queue_adapter = ActiveJob::QueueAdapters::AsyncAdapter.new(
+      min_threads: 1,
+      max_threads: 1,
+      idletime: 60.seconds
+    )
   end
 end
--- config/initializers/activejob.rb
+++
@@ -1,8 +0,0 @@
-# Configure Rails to use the Async adapter but only process one job at a time
-# This sould be fine for production at this point but we could/should switch
-# to something like sidekiq when we can
-Rails.config.active_job.queue_adapter = ActiveJob::QueueAdapters::AsyncAdapter.new(
-  min_threads: 1,
-  max_threads: 1,
-  idletime: 60.seconds
-)
