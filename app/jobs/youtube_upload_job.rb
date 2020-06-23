a068a0f2781d377fc75b9d9903802cc700f509a6
Gavin Stark <gavin@gstark.com>
2020-06-23 14:06:17 -0400

    Limit video processing only to videos that start with a PLAYLISTNAME: to keep out video recordings that don't need to be there

--- app/jobs/youtube_upload_job.rb
+++ app/jobs/youtube_upload_job.rb
@@ -2,8 +2,24 @@ class YoutubeUploadJob < ApplicationJob
   queue_as :default
 
   def perform(url:, topic:)
-    account = Yt::Account.new refresh_token: Rails.application.credentials.YOUTUBE_REFRESH_TOKEN
+    account = Yt::Account.new(refresh_token: Rails.application.credentials.YOUTUBE_REFRESH_TOKEN)
 
-    account.upload_video url, title: topic
+    # Unless there is a : in the topic, return
+    return unless topic.includes?(':')
+
+    # Take the name of the playlist as what comes before the ':'
+    playlist_name = topic.split(':').first
+
+    # If we have one
+    if playlist_name
+      # Find the playlist with that name
+      youtube_playlist = account.playlists.find { |playlist| playlist.title == playlist_name }
+
+      # If we have a matching playlist then upload the video and associate it
+      if youtube_playlist
+        video = account.upload_video(url, title: topic, privacy_status: 'unlisted')
+        youtube_playlist.add_video(video.id)
+      end
+    end
   end
 end
