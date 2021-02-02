88ca23ae37dc9f1d2bd736b19778efae74359828
Gavin Stark <gavin@gstark.com>
2021-02-02 17:53:31 -0500

    Updates the lecture video to make the lecture dependency optional

--- app/models/lecture_video.rb
+++ app/models/lecture_video.rb
@@ -1,6 +1,7 @@
 class LectureVideo < ApplicationRecord
   belongs_to :cohort
-  belongs_to :lecture
+  belongs_to :lecture, required: false
+  has_many :lecture_video_playbacks, dependent: :destroy
 
   has_one_attached :video
 end
