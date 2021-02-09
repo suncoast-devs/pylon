a438acbac944018520b0ae3507a15f48a3ff84f0
Gavin Stark <gavin@gstark.com>
2021-02-09 14:45:09 -0500

    Save the assignment before generating the pylon notice so the URL is complete

--- app/jobs/homework_assigned_job.rb
+++ app/jobs/homework_assigned_job.rb
@@ -4,9 +4,13 @@ class HomeworkAssignedJob < ApplicationJob
   NEXUS_BASE_URL = ENV["NEXUS_BASE_URL"]
 
   def perform(homework:)
-    homework.cohort.student_enrollments.can_be_assigned_homework.includes(:person).each do |enrollment|
-      homework.assignments.find_or_create_by(person: enrollment.person) do |assignment|
-        PylonBot.message(enrollment.person, %{You have a new assignment: *"#{homework.title}"*: #{NEXUS_BASE_URL}/assignments/#{assignment.id} })
+    enrollments = homework.cohort.student_enrollments.can_be_assigned_homework.includes(:person)
+
+    enrollments.each do |enrollment|
+      # TODO: Use previously_new_record? when we update to Rails 6.1
+      homework.assignments.find_or_initialize_by(person: enrollment.person) do |assignment|
+        assignment.save
+        PylonBot.message(enrollment.person, %{You have a new assignment: *"#{homework.title}"*: #{NEXUS_BASE_URL}/assignments/#{assignment.id}})
       end
     end
   end
